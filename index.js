8df18e4af3179990f4556c8874aa86a317b35eab
rritesht <49843780+rritesht@users.noreply.github.com>
2021-06-10 23:48:37 +0530

    changes

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("hello world")
+// alert("virus")
+document.write("this is sccript page")
+console.warn("virusssss")
+console.error("This is an error")
+
+
+// Data type in javaScript
+// Numbers
+var num1 = 452;
+var num2 = 342;
+
+// string
+var str1 = "this is first string"
+var str2 = "This is second string"
+
+// Objects
+var marks = {
+    ravi: 45,
+    subham: 22,
+    Hemant: 55,
+
+}
+console.log(marks)
+
+// Boolean
+var a = true;
+var b = false;
+    console.log(a,b)
+
+/*  At a very high level there are two types of data type in javascript   
+1.Primtive Data types -- string,boolean,numbers,undefined,null,symbol
+2. Reference Data types -- Arrays,objects
+*/
+
+// Array
+var arr = [1,2,3,"abbbu",4,5,]
+
+// Operator in javascript
+// Arithmetic Operators
+var a =34;
+var b =67;
+console.log("The value of a+b is", a+b)
+console.log("The value of a-b is", a-b)
+console.log("The value of a*b is", a*b)
+console.log("The value of a/b is", a/b)
+console.log("The value of a**b is", a**b)
