b6de1a4057f4b731b56d43f21a52aa91780aa658
rritesht <49843780+rritesht@users.noreply.github.com>
2021-06-17 23:53:00 +0530

    new

--- Nmae-Entry/name.js
+++ Nmae-Entry/name.js
@@ -1,11 +1,34 @@
-var data = []
+var data = [];
+
 document.getElementById("demo").innerHTML = data;
 function myfunction() {
-    data.push(document.getElementById("mytext").value);
-    document.getElementById("mytext").value = "";
+
+    var employee = {
+        name: document.getElementById("mytext").value
+    }
+    if (employee.name !== '') {
+        data.push(employee);
+        window.sessionStorage.setItem('employeeList', JSON.stringify(data))
+        document.getElementById("mytext").value = "";
+    }
 
 }
 function myfunction1() {
-    document.getElementById("demo").innerHTML = data;
+    var employeeList = JSON.parse(window.sessionStorage.getItem('employeeList'));
+    data = [];
+    console.log(employeeList)
+    // employeeList.forEach((emp) => {
+    //     document.getElementById("demo").innerHTML = "Name : " + emp.name;
+    // });
+    if (employeeList && employeeList.length) {
+        for (let i = 0; i < employeeList.length; i++) {
+            document.getElementById("demo").innerHTML += "Name : " + employeeList[i].name + "<br>";
+        }
+    }else{
+        document.getElementById("demo").innerHTML = "";
+    }
+}
 
+function onClear() {
+    window.sessionStorage.clear();
 }
